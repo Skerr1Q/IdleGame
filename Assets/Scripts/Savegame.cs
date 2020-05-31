bae8bada46ecac123f8a52c1d767a7228942a2c4
ArtemKuvshinets <kuvshinets@gmail.com>
2020-05-31 04:29:16 +0300

    save

--- Assets/Scripts/Savegame.cs
+++ Assets/Scripts/Savegame.cs
@@ -12,11 +12,23 @@ public class SaveGame : MonoBehaviour
  
     public void SaveTheGame()
     {
-        PlayerPrefs.SetInt("SavedStage", stageMax);
-        PlayerPrefs.SetInt("SavedMoney", coins);
-        PlayerPrefs.SetInt("SavedClick", click);
-        PlayerPrefs.SetInt("SavedJimmyUpgrades", Jimmy);
-        PlayerPrefs.SetInt("SavedSheenUpgrades", Sheen);
-        PlayerPrefs.SetInt("SavedCarlUpgrades", Carl);
+        PlayerPrefs.SetInt("SavedStage", StageMax);
+        PlayerPrefs.SetInt("SavedMoney", Coins);
+
+        PlayerPrefs.SetFloat("SavedClickDamage", ClickUpgrade.Damage);
+        PlayerPrefs.SetInt("SavedClickCost", ClickUpgrade.Cost);
+        PlayerPrefs.SetInt("SavedClickDamage", ClickUpgrade.Level);
+
+        PlayerPrefs.SetFloat("SavedJimmyDPC", JimmyUpgrade.DPC);
+        PlayerPrefs.SetInt("SavedJimmyCost", JimmyUpgrade.Cost);
+        PlayerPrefs.SetInt("SavedJimmyLevel", JimmyUpgrade.Level);
+
+        PlayerPrefs.SetFloat("SavedSheenUpgrades", SheenUpgrade.DPC);
+        PlayerPrefs.SetInt("SavedSheenCost", SheenUpgrade.Cost);
+        PlayerPrefs.SetInt("SavedSheenLevel", SheenUpgrade.Level);
+
+        PlayerPrefs.SetFloat("SavedCarlUpgrades", CarlUpgrade.DPC);
+        PlayerPrefs.SetInt("SavedCarlCost", CarlUpgrade.Cost);
+        PlayerPrefs.SetInt("SavedCarlLevel", CarlUpgrade.Level);
     }
 }
