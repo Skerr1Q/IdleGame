ab90ef84bfb5bebaba1b72a217585dc8a774531a
Vlad Kostenko <vladkost1121@gmail.com>
2020-05-31 05:55:45 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/Skerr1Q/IdleGame""
    This reverts commit 4aed1863f4b181e486e11d6abb9f6cf4d00dbf78.

---
+++ Assets/Scripts/Savegame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Engine;
+using Unity.Engine.UI;
+
+
+public class SaveGame : MonoBehaviour
+{
+ 
+    public void SaveTheGame()
+    {
+        PlayerPrefs.SetInt("SavedStage", stageMax);
+        PlayerPrefs.SetInt("SavedMoney", coins);
+        PlayerPrefs.SetInt("SavedClick", click);
+        PlayerPrefs.SetInt("SavedJimmyUpgrades", Jimmy);
+        PlayerPrefs.SetInt("SavedSheenUpgrades", Sheen);
+        PlayerPrefs.SetInt("SavedCarlUpgrades", Carl);
+    }
+}
