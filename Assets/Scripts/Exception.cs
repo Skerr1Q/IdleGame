43ba34ce279847b06dcc814fb2cec237965a83c0
ArtemKuvshinets <kuvshinets@gmail.com>
2020-06-02 04:35:52 +0300

    Exception

---
+++ Assets/Scripts/Exception.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+class Exception
+{
+    static void Process(int stage, int stageMax)
+    {
+        if(stage > stageMax)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+    static void Main()
+    {
+        try
+        {
+            Process(stage, stageMax);
+        }
+        catch(ArgumentOutOfRangeException e )
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+}
--- Assets/Scripts/Savegame.cs
+++ Assets/Scripts/Savegame.cs
@@ -10,43 +10,40 @@ using UnityEngine.UI;
 
 public class SaveGame : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public void Save()
     {
-<<<<<<< HEAD
+
         PlayerPrefs.SetInt("SavedStage", stageMax);
         PlayerPrefs.SetInt("SavedMoney", coins);
         PlayerPrefs.SetInt("SavedClick", click);
         PlayerPrefs.SetFloat("SavedJimmyDPS", JimmyUpgrade.DPS);
         PlayerPrefs.SetFloat("SavedSheenDPS", SheenUpgrade.DPS);
         PlayerPrefs.SetFloat("SavedCarlDPS", CarlUpgrade.DPS);
-=======
-=======
- 
-    public void Save()
-    {
->>>>>>> df47c0f397aff8a0ffe94d1348fb7f908b58f474
-        PlayerPrefs.SetInt("SavedStage", StageMax);
-        PlayerPrefs.SetInt("SavedMoney", Coins);
 
-        PlayerPrefs.SetFloat("SavedClickDamage", ClickUpgrade.Damage);
-        PlayerPrefs.SetInt("SavedClickCost", ClickUpgrade.Cost);
-        PlayerPrefs.SetInt("SavedClickDamage", ClickUpgrade.Level);
 
-        PlayerPrefs.SetFloat("SavedJimmyDPC", JimmyUpgrade.DPC);
-        PlayerPrefs.SetInt("SavedJimmyCost", JimmyUpgrade.Cost);
-        PlayerPrefs.SetInt("SavedJimmyLevel", JimmyUpgrade.Level);
+        public void Save()
+        {
 
-        PlayerPrefs.SetFloat("SavedSheenUpgrades", SheenUpgrade.DPC);
-        PlayerPrefs.SetInt("SavedSheenCost", SheenUpgrade.Cost);
-        PlayerPrefs.SetInt("SavedSheenLevel", SheenUpgrade.Level);
+            PlayerPrefs.SetInt("SavedStage", StageMax);
+            PlayerPrefs.SetInt("SavedMoney", Coins);
 
-        PlayerPrefs.SetFloat("SavedCarlUpgrades", CarlUpgrade.DPC);
-        PlayerPrefs.SetInt("SavedCarlCost", CarlUpgrade.Cost);
-        PlayerPrefs.SetInt("SavedCarlLevel", CarlUpgrade.Level);
-<<<<<<< HEAD
->>>>>>> bae8bada46ecac123f8a52c1d767a7228942a2c4
-=======
->>>>>>> df47c0f397aff8a0ffe94d1348fb7f908b58f474
+            PlayerPrefs.SetFloat("SavedClickDamage", ClickUpgrade.Damage);
+            PlayerPrefs.SetInt("SavedClickCost", ClickUpgrade.Cost);
+            PlayerPrefs.SetInt("SavedClickDamage", ClickUpgrade.Level);
+
+            PlayerPrefs.SetFloat("SavedJimmyDPC", JimmyUpgrade.DPC);
+            PlayerPrefs.SetInt("SavedJimmyCost", JimmyUpgrade.Cost);
+            PlayerPrefs.SetInt("SavedJimmyLevel", JimmyUpgrade.Level);
+
+            PlayerPrefs.SetFloat("SavedSheenUpgrades", SheenUpgrade.DPC);
+            PlayerPrefs.SetInt("SavedSheenCost", SheenUpgrade.Cost);
+            PlayerPrefs.SetInt("SavedSheenLevel", SheenUpgrade.Level);
+
+            PlayerPrefs.SetFloat("SavedCarlUpgrades", CarlUpgrade.DPC);
+            PlayerPrefs.SetInt("SavedCarlCost", CarlUpgrade.Cost);
+            PlayerPrefs.SetInt("SavedCarlLevel", CarlUpgrade.Level);
+
+        }
     }
 }
